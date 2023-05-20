42793ffcdcda13e12425d92dfd362c5dfd05b30b
serchg0george <serchg0george@gmail.com>
2023-05-20 20:48:35 +0300

    Added: To week_one.stream_api.Task7, ReadingAndSortingFromFile Was done: To read the file, removing the empty lines and creating a collection of objects with the information from the line; From the created collection, display the person with the longest name (firstName + lastName); Count the people living on street number 25.

--- src/week_one/stream_api/Task7/ReadingAndSortingFromFile.java
+++ src/week_one/stream_api/Task7/ReadingAndSortingFromFile.java
@@ -16,23 +16,45 @@ public class ReadingAndSortingFromFile {
                     .filter(line -> !line.isBlank())
                     .collect(Collectors.toList());
 
+
             List<Integer> sumOfCharactersList = new ArrayList<>();
             String firstNameMax = "";
             String lastNameMax = "";
+
+            String firstNameStreets = "";
+            String lastNameStreets = "";
+
+
 
             for (String dataFromFile :
                     insertedDataFromFile) {
                 String[] parts = dataFromFile.split(",");
                 String[] firstNameLastNameFromFile = Arrays.copyOfRange(parts, 0, 2);
 
+                String[] numberOfStreet = Arrays.copyOfRange(parts, 0, 6);
+
+
                 String rawFirstName = firstNameLastNameFromFile[0].trim();
                 String rawLastName = firstNameLastNameFromFile[1].trim();
+
+
+                String rawNumberOfStreet = numberOfStreet[5].trim();
+
 
                 String extractedFirstName = rawFirstName.substring(rawFirstName.indexOf("=") + 1);
                 String extractedLastName = rawLastName.substring(rawLastName.indexOf("=") + 1);
 
+
+                Integer extractedNumberOfStreet = Integer.parseInt(rawNumberOfStreet.substring(rawNumberOfStreet.indexOf("=") + 1));
+
                 int sumOfCharacters = extractedFirstName.length() + extractedLastName.length();
                 sumOfCharactersList.add(sumOfCharacters);
+
+                if (extractedNumberOfStreet.equals(25)) {
+                    firstNameStreets = extractedFirstName;
+                    lastNameStreets = extractedLastName;
+                    System.out.println("Lives on street with number 25: " + firstNameStreets + " " + lastNameStreets);
+                }
 
                 if (sumOfCharacters == Collections.max(sumOfCharactersList)) {
                     firstNameMax = extractedFirstName;
@@ -46,4 +68,5 @@ public class ReadingAndSortingFromFile {
         }
 
     }
+
 }
